68fe650cace2429c09d179c39cf39c8e424b3601
goolman <xdockalr@gmail.com>
2018-01-28 22:53:02 +0100

    S02.02 Menus

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,6 +18,9 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -42,6 +45,24 @@ public class MainActivity extends AppCompatActivity {
 
         fetchWeatherData();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            mWeatherTV.setText("");
+            fetchWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public class getWeatherDataAsync extends AsyncTask <String, Void, String[]> {
